c6eccc08f963d168fd5bcb552480e10f61665102
vdsalex <alexandre.vandensteen@gmail.com>
2022-04-30 16:06:41 +0200

    initialize project

---
+++ src/main/kotlin/com/vds/kultraback/KUltraBackApplication.kt
@@ -0,0 +1,11 @@
+package com.vds.kultraback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KUltraBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<KUltraBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/vds/kultraback/KUltraBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vds.kultraback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KUltraBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
